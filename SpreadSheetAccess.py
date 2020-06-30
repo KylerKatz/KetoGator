6e56817890e6550cf2eb39c1e52f490b090ad85c
rpantoj <rpantoja@ufl.edu>
2020-06-30 11:52:20 -0400

    Created functions to create missing directories and missing anthropometrics table

--- SpreadSheetAccess.py
+++ SpreadSheetAccess.py
@@ -20,32 +20,58 @@ def getAllPatients():
 def getPatientGraphs(patient):
     return (os.listdir("./Current Patients/"+patient+"/DataBases/Data/"))
 
+# Do a check to see if the Patient has an Anthropoemtics table
+# If not, create one with the naming scheme MrNumber_Anthropometrics_Source.xlsx
+# Add the columns at the top 
+# Then input the data
+# If so, this is fine 
 def saveAnthropometrics(patient, MrNumber,Date,DayType,Source,CP,PA,Ht,Wt,HC,UAC,TSF,SSF,USF,SIS,MBSF,UC,Entered,Comments):
-    
-    # Do a check to see if the Patient has an Anthropoemtics tabel 
-
-    # If not, create one with the naming scheme MrNumber_Anthropometrics_Source.xlsx
-
-    # Add the columns at the top 
-
-    # Then input the data
-
-    # If so, this is fine 
     path = r"Current Patients\\" + patient + r"\\DataBases\\Data\\" + patient + r"_Anthropometrics_Source.xlsx"
-    wb = load_workbook(path)
-    ws = wb['Anthropometrics']
+    
+    # Tries to load workbook
+    try:
+        wb = load_workbook(path)
+        ws = wb['Anthropometrics']
+    # On failure ensures they have the right directories using setNewPatient
+    except FileNotFoundError:
+        setNewPatient(patient)
 
+        # Create new workbook and name sheet
+        wb = Workbook()
+        ws = wb.active
+        ws.title = "Anthropometrics"
+
+        # Create appropriate columns
+        ws.append(["MrNumber","Date","Day_Type","Source","CP","PA","Ht","Wt","HC","UAC","TSF","SSF","USF","SIS","MBSF","UC", "R", "X", "Entered", "Audited", "Comments"])
+
+    # Add data
     ws.append([MrNumber,Date,DayType,Source,CP,PA,Ht,Wt,HC,UAC,TSF,SSF,USF,SIS,MBSF,UC, '', '', Entered, '', Comments])
 
+    #Save modified workbook
     wb.save(path)
 
+# Returns dataframe containing data inside the excel sheet
 def getAnthropometricsDataFrame(patient):
     path = r"Current Patients\\" + patient + r"\\DataBases\\Data\\" + patient + r"_Anthropometrics_Source.xlsx"
     dataframe = pandas.read_excel(path)
-
     return dataframe
 
+# Creates appropriate directories for new patient or existing patient who doesn't have them
 def setNewPatient(MrNum):
-    # Create a new Folder for this patient 
+    path = r"Current Patients\\" + MrNum + r"\\DataBases\\Data\\"
 
-    # Create the DataBases and Data Folders 
+    # If path doesn't exist, the function executes, otherwise nothing happens
+    if not os.path.isdir(path):
+        
+        # Used to make appropriate directories
+        switch = {
+            0: MrNum,
+            1: "\DataBases",
+            2: "\Data",
+        }
+        dir = "Current Patients\\"
+
+        # Create necessary directories
+        for i in range(3):
+            dir = dir + switch.get(i)
+            os.mkdir(dir)
